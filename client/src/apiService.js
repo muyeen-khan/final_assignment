3b7b8699fb6c43026a5fe2079f6f355e01e5e7aa
Muyeen Khan <muyeenkhan80@gmail.com>
2025-02-08 21:38:52 +0600

    prepared for submission

--- client/src/apiService.js
+++ client/src/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://assignment.muyeenkhan.com",
+  baseURL: "http://localhost:3000",
   headers: {
     "Content-type": "application/json",
   },
